9490ec6dad7a40d50722454b39ebe1a0406d76eb
herrnorbert <nkobli@yahoo.com>
2018-07-23 17:22:22 +0200

    feat(app): use firefoxsurfer class

--- src/App.java
+++ src/App.java
@@ -1,36 +1,10 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
-import java.util.concurrent.TimeUnit;
-
 public class App {
 
   public static final String BASE_URL = "http://9gag.com";
 
   public static void main(String[] args) {
     System.setProperty("webdriver.gecko.driver","/home/norbi/Documents/geckodriver");
-    WebDriver webDriver = new FirefoxDriver();
-
-    webDriver.manage().window().fullscreen();
-
-    webDriver.get(BASE_URL);
-
-    webDriver.findElement(By.className("closebutton_closeButton--3abym")).click();
-
-    try {
-      while (true){
-        long delay = 5;
-        webDriver.findElement(By.tagName("body")).sendKeys("j");
-        if(webDriver.findElement(By.className("main-wrap")).findElements(By.className("gif-post")) != null){
-          delay = 15;
-        }
-        TimeUnit.SECONDS.sleep(delay);
-      }
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    } finally {
-      webDriver.quit();
-    }
+    FirefoxSurfer surfer = new FirefoxSurfer(false, 5, 3);
+    surfer.surf();
   }
 }
