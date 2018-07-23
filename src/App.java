54a459bc8c3330420500f880612de28c64175cf7
herrnorbert <nkobli@yahoo.com>
2018-07-23 14:43:28 +0200

    feat(app): add firefox web driver

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class App {
+
+  public static final String BASE_URL = "http://9gag.com";
+
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    System.setProperty("webdriver.gecko.driver","/home/norbi/Documents/geckodriver");
+    WebDriver webDriver = new FirefoxDriver();
+
+    webDriver.get(BASE_URL);
+    webDriver.manage().window().fullscreen();
+
   }
 }
